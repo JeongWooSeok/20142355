798cc8abed5b74771255bd8569b78d31f8779292
이름 성 <20142355@vision.hoseo.edu>
2018-11-12 03:53:33 +0900

    PP07.TextureManager

--- GameFramework/PP01.HelloSDL/Game.h
+++ GameFramework/PP01.HelloSDL/Game.h
@@ -1,22 +1,46 @@
-#pragma once
-#include <iostream>
-#include "SDL.h"
-class Game
+#include "TextureManager.h"
+bool TextureManager::load(std::string fileName, std::string id,
+	SDL_Renderer* pRenderer)
 {
-public:
-	Game() {}
-	~Game() {}
-	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
-	void render();
-	void update();
-	void handleEvents();
-	void clean();
-	bool running() { return m_bRunning; }
-private:
-	SDL_Window * m_pWindow;
-	SDL_Renderer* m_pRenderer;
-	SDL_Texture* m_pTexture; // the new SDL_Texture variable
-	SDL_Rect m_sourceRectangle; // 
-	SDL_Rect m_destinationRectangle; // 
-	bool m_bRunning;
-};
+	SDL_Surface* pTempSurface = IMG_Load(fileName.c_str());
+	if (pTempSurface == 0) {
+		return false;
+	}
+	SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer, pTempSurface);
+	SDL_FreeSurface(pTempSurface);
+	if (pTexture != 0) {
+		m_textureMap[id] = pTexture;
+		return true;
+	}
+	return false;
+}
+void TextureManager::draw(std::string id,
+	int x, int y, int width, int height,
+	SDL_Renderer* pRenderer, SDL_RendererFlip flip)
+{
+	SDL_Rect srcRect;
+	SDL_Rect destRect;
+	srcRect.x = 0;
+	srcRect.y = 0;
+	srcRect.w = destRect.w = width;
+	srcRect.h = destRect.h = height;
+	destRect.x = x;
+	destRect.y = y;
+	SDL_RenderCopyEx(pRenderer, m_textureMap[id],
+		&srcRect, &destRect, 0, 0, flip);
+}
+void TextureManager::drawFrame(std::string id, int x, int y,
+	int width, int height, int currentRow, int currentFrame,
+	SDL_Renderer* pRenderer, SDL_RendererFlip flip)
+{
+	SDL_Rect srcRect;
+	SDL_Rect destRect;
+	srcRect.x = width * currentFrame;
+	srcRect.y = height * (currentRow - 1);
+	srcRect.w = destRect.w = width;
+	srcRect.h = destRect.h = height;
+	destRect.x = x;
+	destRect.y = y;
+	SDL_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect,
+		&destRect, 0, 0, flip);
+}
